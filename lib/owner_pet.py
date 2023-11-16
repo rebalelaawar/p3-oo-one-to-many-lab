c03c4b9cd61f2b099f921a08ebafdce9d1bdf1c4
rebalelaawar <rebalelaawar@gmail.com>
2023-11-16 14:40:01 -0500

    One to many

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,50 @@
 class Pet:
-    pass
+
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+
+    all = []
+
+
+
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
+    
+    @property
+    def pet_type(self):
+        return self._pet_type
+    
+    @pet_type.setter
+    def pet_type(self, new_pet_type):
+        if new_pet_type not in Pet.PET_TYPES:
+            raise Exception("Not a valid pet type")
+        else:
+            self._pet_type = new_pet_type
+        
+
 
 class Owner:
-    pass
+    def __init__(self,name):
+        self.name = name
+    
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner ==self]
+    
+    def add_pet(self, new_pet):
+        if isinstance(new_pet, Pet):
+            new_pet.owner = self
+        else:
+            raise Exception("NO")
+
+    def get_sorted_pets(self):
+
+        my_pets = self.pets()
+        
+        return sorted(my_pets, key = lambda each_pet: each_pet.name.lower() )
+       
+        
+
+        
